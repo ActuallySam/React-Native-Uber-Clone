c19a80a39fd6b22f16933df327b067005db8219c
ActuallySam <srivastava.samarth.btech2018@sitpune.edu.in>
2021-09-05 20:46:05 +0530

    project initiation

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from "react-redux";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
     </View>
   );
 }
